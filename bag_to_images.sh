1e76631ed8f4f66f56b7b4b5f88b143605e8b452
yipuzhao <yzhao347@gatech.edu>
2017-11-08 11:51:08 -0500

    update how to guide

--- bag_to_images.sh
+++ bag_to_images.sh
@@ -1,6 +1,6 @@
 export DATA_PATH=/mnt/DATA/Datasets/GazeboMaze
 
-export BAG_NAME=brick_wall
+export BAG_NAME=ceiling_tiled
 
 mkdir $DATA_PATH/$BAG_NAME
 mkdir $DATA_PATH/$BAG_NAME/cam0
@@ -8,4 +8,4 @@ mkdir $DATA_PATH/$BAG_NAME/cam0/data/
 mkdir $DATA_PATH/$BAG_NAME/cam1
 mkdir $DATA_PATH/$BAG_NAME/cam1/data/
 python bag_to_images.py $DATA_PATH/$BAG_NAME/$BAG_NAME.bag $DATA_PATH/$BAG_NAME/cam0/data/ /multisense_sl/camera/left/image_raw
-python bag_to_images.py $DATA_PATH/$BAG_NAME/$BAG_NAME.bag $DATA_PATH/$BAG_NAME/cam1/data/ multisense_sl/camera/right/image_raw
+python bag_to_images.py $DATA_PATH/$BAG_NAME/$BAG_NAME.bag $DATA_PATH/$BAG_NAME/cam1/data/ /multisense_sl/camera/right/image_raw
