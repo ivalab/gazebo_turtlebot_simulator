37a8ab027ae4830e23c2f4c98e1220f39057b1ac
yipuzhao <yipu.zhao@gatech.edu>
2018-03-09 01:44:48 -0500

    fix issue in generating track from svo2 rosbag

--- pose_extractor_svo.m
+++ pose_extractor_svo.m
@@ -27,7 +27,7 @@ for bn = 1 % 1:length(bag_name)
   
   for rn = 1 : round_num
     
-    bag = rosbag([bag_path 'Round' num2str(rn) '/' bag_name{bn} '_svo_new.bag'])
+    bag = rosbag([bag_path 'Round' num2str(rn) '/' bag_name{bn} '_svo.bag'])
     %     bagselect = select(bag, 'Topic', '/svo/pose_cam/0')
     bagselect = select(bag, 'Topic', '/tf')
     clear bag
