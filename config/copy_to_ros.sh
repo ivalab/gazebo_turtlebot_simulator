6b2c33df57d2c2237c98deb610d0738846260fed
Yipu Zhao <yipu.zhao@gatech.edu>
2018-09-20 14:21:01 -0400

    add adapt script for new ros kinectic

---
+++ config/copy_to_ros.sh
@@ -0,0 +1,5 @@
+sudo cp /home/yipuzhao/catkin_ws/src/gazebo_stereo_simulator/config/kobuki_ADIS_16448* /opt/ros/kinetic/share/kobuki_description/urdf
+sudo cp /home/yipuzhao/catkin_ws/src/gazebo_stereo_simulator/config/kobuki_mpu_6000* /opt/ros/kinetic/share/kobuki_description/urdf
+#
+sudo cp /home/yipuzhao/catkin_ws/src/gazebo_stereo_simulator/config/kobuki_hexagons_* /opt/ros/kinetic/share/turtlebot_description/robots
+
