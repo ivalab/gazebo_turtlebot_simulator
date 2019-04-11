ba328bb385f0a714037e242bb505edf46d56610f
Yipu Zhao <yipu.zhao@gatech.edu>
2019-04-11 18:31:43 -0400

    update

--- set_up_sim.sh
+++ set_up_sim.sh
@@ -1,5 +1,6 @@
 sudo apt install ros-kinetic-kobuki-gazebo
 sudo apt install ros-kinetic-tf2-sensor-msgs
+sudo apt install ros-kinetic-navigation
 
 export CATKIN_WS=/home/yipuzhao/catkin_ws/
 config/copy_to_ros.sh
