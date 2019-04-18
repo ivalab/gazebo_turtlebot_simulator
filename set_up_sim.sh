5224f42d47d2b29cc1f7c3aa1a28cb58f8bf7046
yipuzhao <yipu.zhao@gatech.edu>
2019-04-18 22:23:59 +0000

    update

--- set_up_sim.sh
+++ set_up_sim.sh
@@ -1,6 +1,7 @@
+sudo apt install ros-${ROS_DISTRO}-turtlebot-description
 sudo apt install ros-${ROS_DISTRO}-kobuki-gazebo
 sudo apt install ros-${ROS_DISTRO}-tf2-sensor-msgs
 sudo apt install ros-${ROS_DISTRO}-navigation
 
-export CATKIN_WS=/home/yipuzhao/catkin_ws/
+export CATKIN_WS=/home/nvidia/catkin_ws/
 config/copy_to_ros.sh
