e4dc1293f3330a4e12500f54bce96a1fe2cd5bd9
yipuzhao <zhaoyipu@gmail.com>
2020-02-09 17:21:03 -0800

    add missing dep in auto setup script

--- set_up_sim.sh
+++ set_up_sim.sh
@@ -1,7 +1,10 @@
+sudo apt install ros-${ROS_DISTRO}-turtlebot-bringup
 sudo apt install ros-${ROS_DISTRO}-turtlebot-description
 sudo apt install ros-${ROS_DISTRO}-kobuki-gazebo
 sudo apt install ros-${ROS_DISTRO}-tf2-sensor-msgs
 sudo apt install ros-${ROS_DISTRO}-navigation
+sudo apt install ros-${ROS_DISTRO}-joy*
+sudo apt install autoconf
 
 export CATKIN_WS=/home/yipuzhao/catkin_ws/
 config/copy_to_ros.sh
